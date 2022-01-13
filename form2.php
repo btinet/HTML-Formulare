3487010de8b11866fb55818cba8a4618153b12ec
Benjamin Wagner <bwagner@vapita.de>
2022-01-13 11:41:14 +0100

    form class added

--- form2.php
+++ form2.php
@@ -8,36 +8,146 @@
 <body class="container">
 <?php
 include ('menu.php');
+
+class Form
+{
+    protected array $items;
+    protected array $buttons;
+    protected string $method;
+    protected string $action;
+
+    public function __construct($method = 'get', $action = false)
+    {
+        $this->setMethod($method);
+        $this->setAction($action);
+    }
+
+    public function addInputField($label, $name,$type,array $attributes = []):void
+    {
+        $formRow = "<input type='{$type}' name='{$name}'";
+        if($attributes)
+        {
+            foreach ($attributes as $attribute => $value)
+            {
+                $formRow .= " {$attribute}='{$value}' ";
+            };
+        }
+        $formRow .= ">";
+        $formLabelRow = "<label>{$label}{$formRow}</label>";
+        $this->setItems($formLabelRow);
+    }
+
+    public function addButton($label, $name, $type,array $attributes = []):void
+    {
+        $button = "<button type='{$type}' name='{$name}'";
+        if($attributes) {
+            foreach ($attributes as $attribute => $value) {
+                $button .= " {$attribute}='{$value}' ";
+            };
+        }
+        $button .= ">{$label}</button>";
+        $this->setButtons($button);
+    }
+
+    public function render():string
+    {
+        echo "<form 
+            method='{$this->getMethod()}' 
+            action='{$this->getAction()}'>" .
+            implode(' ', $this->getItems()) .
+            implode(' ', $this->getButtons()) .
+            '</form>';
+    }
+
+    /**
+     * @return array
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param string $item
+     */
+    public function setItems(string $item): void
+    {
+        $this->items[] = $item;
+    }
+
+    /**
+     * @return array
+     */
+    public function getButtons(): array
+    {
+        return $this->buttons;
+    }
+
+    /**
+     * @param string $button
+     */
+    public function setButtons(string $button): void
+    {
+        $this->buttons[] = $button;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+
+    /**
+     * @param string $method
+     */
+    public function setMethod(string $method): void
+    {
+        $this->method = $method;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAction(): string
+    {
+        return $this->action;
+    }
+
+    /**
+     * @param string $action
+     */
+    public function setAction(string $action): void
+    {
+        $this->action = $action;
+    }
+
+}
+
+$formItems = [
+
+];
 ?>
 
     <h1>Surfresort-Buchung</h1>
-    <form method="post">
-        <label>
-            Name
-            <input type="text" name="lastname" value="<?= $_POST['lastname'] ?: '' ?>">
-        </label>
-        <label>
-            Vorname
-            <input type="text" name="firstname" value="<?= $_POST['firstname'] ?: '' ?>">
-        </label>
-        <label>
-            Adresse
-            <input type="text" name="address_1" value="<?= $_POST['address_1'] ?: '' ?>">
-        </label>
-        <label>
-            Adresse 2
-            <input type="text" name="address_2" value="<?= $_POST['address_2'] ?: '' ?>">
-        </label>
-        <label>
-            Anreise
-            <input type="date" name="date_arrival">
-        </label>
-        <label>
-            Abreise
-            <input type="date" name="date_departure">
-        </label>
-        <button type="submit">Absenden</button>
-    </form>
+    <?php
+
+    $form = new Form('post');
+    $form->addInputField('Vorname','firstname','text',[
+            'required' => 'required'
+    ]);
+    $form->addInputField('Nachname','firstname','text');
+    $form->addInputField('Passwort','password','password');
+    $form->addInputField('Straße und Hausnummer','address','text');
+    $form->addInputField('Postleitzahl','plz','text');
+    $form->addInputField('Ort','city','text');
+    $form->addInputField('Anreise','arrival','date');
+    $form->addInputField('Abreise','departure','date');
+    $form->addButton('Absenden','submit','submit');
+    $form->render();
+
+    ?>
 
 
 <h2>Formularausgabe</h2>
